8cd17418e3412691594fa6504458c506773ba7d2
sTeeLM <steel.mental@gmail.com>
2024-12-10 12:09:27 +0800

    improve key scan

--- Source/xmeter/button.c
+++ Source/xmeter/button.c
@@ -9,20 +9,18 @@
 #include "beeper.h"
 #include "gpio.h"
 
-
-#define KEY_LPRESS_DELAY 2 // 长按时间，2s
+#define KEY_LPRESS_DELAY 1 // 长按时间
 
 static bit mod_down;
 static bit set_down;
-static bit mod_pressed;
-static bit set_pressed;
-static bit mod_set_pressed_send;
-static bit mod_set_pressed;
-static uint8_t set_down_sec;
-static uint8_t mod_down_sec;
-static uint8_t set_down_diff;
-static uint8_t mod_down_diff;
-static uint8_t mod_set_down_diff;
+static uint8_t key_down_cnt;
+static bit mod_lpress_send;
+static bit set_lpress_send;
+static bit mod_set_press_send;
+static bit mod_set_lpress_send;
+static uint32_t set_down_sec;
+static uint32_t mod_down_sec;
+static uint32_t mod_set_down_sec;
 
 void button_initialize(void)
 {
@@ -38,13 +36,13 @@ void button_initialize(void)
   
   PX1 = 0;
   IPH &= ~0x40; /* PX1H = 0 */
-
-  mod_down = 0;
-  set_down = 0;
-  mod_pressed = 0;
-  set_pressed = 0;
-  mod_set_pressed = 0;
-  mod_set_pressed_send = 0;
+  
+  mod_down = set_down = 0;
+  key_down_cnt = 0;
+  mod_lpress_send = 0;
+  set_lpress_send = 0;
+  mod_set_press_send = 0;
+  mod_set_lpress_send = 0;
 }
 
 static void mod_A_ISR (void) interrupt 0 using 1
@@ -78,17 +76,98 @@ static bit key_mod_pressed()
 {
   return KEY_MOD_C == 0;
 }
+
+void button_scan_proc(enum task_events ev)
+{
+  if(key_mod_pressed()) {
+    if(mod_down == 0) {
+      mod_down = 1;
+      key_down_cnt ++;
+      task_set(EV_KEY_MOD_DOWN);
+      mod_down_sec = clock_get_now_sec();
+      if(key_down_cnt == 2) {
+        mod_set_down_sec = mod_down_sec;
+      }
+    }
+  } else {
+    if(mod_down == 1) {
+      if(key_down_cnt == 2) {
+        if(!mod_set_lpress_send) {
+          task_set(EV_KEY_MOD_SET_PRESS);
+          mod_set_press_send = 1;
+        }
+      } else {
+        if(!mod_lpress_send && !mod_set_lpress_send && !mod_set_press_send) {
+          task_set(EV_KEY_MOD_PRESS);
+        }
+       
+      }
+      mod_down = 0;
+      key_down_cnt --;
+      mod_lpress_send = 0;
+      if(key_down_cnt == 0) {
+        mod_set_lpress_send = 0;
+        mod_set_press_send  = 0;
+      }
+      task_set(EV_KEY_MOD_UP);
+    }
+  }
+  
+  if(key_set_pressed()) {
+    if(set_down == 0) {
+      set_down = 1;
+      key_down_cnt ++;
+      task_set(EV_KEY_SET_DOWN);
+      set_down_sec = clock_get_now_sec();
+      if(key_down_cnt == 2) {
+        mod_set_down_sec = set_down_sec;
+      }     
+    }
+  } else {
+    if(set_down == 1) {
+      if(key_down_cnt == 2) {
+        if(!mod_set_lpress_send) {
+          task_set(EV_KEY_MOD_SET_PRESS);
+          mod_set_press_send = 1;
+        }
+      } else {
+        if(!set_lpress_send && !mod_set_lpress_send && !mod_set_press_send) {
+          task_set(EV_KEY_SET_PRESS);
+        }
+      }      
+      set_down = 0;
+      key_down_cnt --;
+      set_lpress_send = 0;
+      if(key_down_cnt == 0) {
+        mod_set_lpress_send = 0;
+        mod_set_press_send  = 0;
+      }
+      task_set(EV_KEY_SET_UP);    
+    }
+  }
+  
+  if(key_down_cnt == 2) {
+    if(clock_diff_now_sec(mod_set_down_sec) > KEY_LPRESS_DELAY) {
+      task_set(EV_KEY_MOD_SET_LPRESS);
+      mod_set_lpress_send = 1;
+    }
+  } else {
+    if(mod_down) {
+      if(clock_diff_now_sec(mod_down_sec) > KEY_LPRESS_DELAY) {
+        task_set(EV_KEY_MOD_LPRESS);
+        mod_lpress_send = 1;
+      }
+    }
+    if(set_down) {
+      if(clock_diff_now_sec(set_down_sec) > KEY_LPRESS_DELAY) {
+        task_set(EV_KEY_SET_LPRESS);
+        set_lpress_send = 1;
+      }
+    }
+  }
+  
+}
 /*
-EV_KEY_MOD_PRESS: mod down，之后在delay时间内up
-EV_KEY_MOD_LPRESS mod down，之后在delay时间外up
-EV_KEY_SET_PRESS: set down，之后在delay时间内up
-EV_KEY_SET_LPRESS set down，之后在delay时间外up
-EV_KEY_MOD_SET_PRESS：mod down & set down， 之后在delay时间内两个都up
-EV_KEY_MOD_SET_LPRESS：mod down & set down， 之后在delay时间外两个都up
-*/
-
-
-
 void button_scan_proc(enum task_events ev)
 {
   if(key_mod_pressed()) {
@@ -175,10 +254,15 @@ void button_scan_proc(enum task_events ev)
     mod_pressed = 0;
   }
 }
+*/
+
 
 void button_mod_proc(enum task_events ev)
 {
   switch (ev) {
+    case EV_KEY_MOD_DOWN:
+      CDBG("button_set_proc EV_KEY_MOD_DOWN\n");  
+      break;      
     case EV_KEY_MOD_C:
       CDBG("button_set_proc EV_KEY_MOD_C\n");  
       break;    
@@ -193,6 +277,9 @@ void button_mod_proc(enum task_events ev)
       beeper_beep_beep();
       CDBG("button_mod_proc EV_KEY_MOD_LPRESS\n");
       break; 
+    case EV_KEY_MOD_UP:
+      CDBG("button_set_proc EV_KEY_MOD_UP\n");  
+      break;  
     default:
       ;
   }
@@ -203,6 +290,9 @@ void button_mod_proc(enum task_events ev)
 void button_set_proc(enum task_events ev)
 {
   switch (ev) {
+    case EV_KEY_SET_DOWN:
+      CDBG("button_set_proc EV_KEY_SET_DOWN\n");  
+      break;     
     case EV_KEY_SET_C:
       CDBG("button_set_proc EV_KEY_SET_C\n");  
       break;    
@@ -217,6 +307,9 @@ void button_set_proc(enum task_events ev)
       beeper_beep_beep();
       CDBG("button_set_proc EV_KEY_SET_LPRESS\n");
       break; 
+    case EV_KEY_SET_UP:
+      CDBG("button_set_proc EV_KEY_SET_UP\n");  
+      break;  
     default:
       ;
   }
--- Source/xmeter/task.h
+++ Source/xmeter/task.h
@@ -9,23 +9,27 @@ enum task_events
   EV_INIT                = 0,
   EV_KEY_SCAN            = 1, // 扫描按键   
   EV_250MS               = 2, // 大约每250ms转一下
-  EV_1S                  = 3, // 大约每1s转一下  
-  EV_KEY_SET_C           = 4, // set顺时针旋转
-  EV_KEY_SET_CC          = 5, // set逆时针旋转  
-  EV_KEY_SET_PRESS       = 6, // set键短按
-  EV_KEY_SET_LPRESS      = 7, // set键长按
-  EV_KEY_MOD_C           = 8, // mod顺时针旋转
-  EV_KEY_MOD_CC          = 9, // mod逆时针旋转  
-  EV_KEY_MOD_PRESS       = 10, // mod键短按
-  EV_KEY_MOD_LPRESS      = 11, // mod键长按
-  EV_KEY_MOD_SET_PRESS   = 12, // mod set 键同时短按
-  EV_KEY_MOD_SET_LPRESS  = 13, // mod set 键同时长按
-  EV_OVER_HEAT           = 14, // 超过最高温度限制，只要超过就会持续发过热消息
-  EV_OVER_PD             = 15, // 计算耗散功率大于最大限制（可能温度计还没有反应过来），并且对外开关处于打开状态
-  EV_TEMP_HI             = 16, // 超过风扇温度上限，并且风扇处于关闭状态
-  EV_TEMP_LO             = 17, // 低于风扇温度下线，并且风扇处于打开状态
-  EV_CC_CHANGE           = 18, // CC状态变化
-  EV_TIMEO               = 19, // 超时  
+  EV_1S                  = 3, // 大约每1s转一下 
+  EV_KEY_SET_DOWN        = 4, // set按下
+  EV_KEY_SET_C           = 5, // set顺时针旋转
+  EV_KEY_SET_CC          = 6, // set逆时针旋转  
+  EV_KEY_SET_PRESS       = 7, // set键短按
+  EV_KEY_SET_LPRESS      = 8, // set键长按
+  EV_KEY_SET_UP          = 9, // set抬起
+  EV_KEY_MOD_DOWN        = 10, // mod按下
+  EV_KEY_MOD_C           = 11, // mod顺时针旋转
+  EV_KEY_MOD_CC          = 12, // mod逆时针旋转  
+  EV_KEY_MOD_PRESS       = 13, // mod键短按
+  EV_KEY_MOD_LPRESS      = 14, // mod键长按
+  EV_KEY_MOD_UP          = 15, // mod抬起  
+  EV_KEY_MOD_SET_PRESS   = 16, // mod set 键同时短按
+  EV_KEY_MOD_SET_LPRESS  = 17, // mod set 键同时长按
+  EV_OVER_HEAT           = 18, // 超过最高温度限制，只要超过就会持续发过热消息
+  EV_OVER_PD             = 19, // 计算耗散功率大于最大限制（可能温度计还没有反应过来），并且对外开关处于打开状态
+  EV_TEMP_HI             = 20, // 超过风扇温度上限，并且风扇处于关闭状态
+  EV_TEMP_LO             = 21, // 低于风扇温度下线，并且风扇处于打开状态
+  EV_CC_CHANGE           = 22, // CC状态变化
+  EV_TIMEO               = 23, // 超时  
   EV_CNT  
 };
 
